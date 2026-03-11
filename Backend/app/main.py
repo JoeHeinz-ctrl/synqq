c3481ba4f73258bfc6d493c8ad3b67c9835c03ca
JoeHeinz-ctrl <jaffijoe03@gmail.com>
2026-03-11 22:25:46 +0530

    Changes made to be deployed 0.3

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -21,7 +21,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://synqq-neon.vercel.app/",
+        "https://synqq-neon.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
